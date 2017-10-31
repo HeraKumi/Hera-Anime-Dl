d7fcd690014a870676a537f5abcf5d9e9e09fe91
Herakumi <heraa@protonmail.com>
2017-10-31 14:54:43 -0700

    Added a more nice interface in the CLI
    just added a few things

--- anime.js
+++ anime.js
@@ -1,6 +1,9 @@
-const input = require('input')
-const chalk = require('chalk')
-const anime = require('anime-dl')
+const input = require('input');
+const anime = require('anime-dl');
+const boxen = require('boxen');
+
+console.log(boxen("Welcome to Hera's Anime Download CLI console application", {padding: 1, margin: 1, borderStyle: 'double'}));
+
 
 async function animeDownload() {
 	const name = await input.text('What anime would you like to download?');
