8909dcc3613210c2345d6fa9395ac649fb08c2ad
Herakumi <heraa@protonmail.com>
2017-11-05 11:21:09 -0800

    Added A loading assets
    loading assets <3

--- Main/anime.js
+++ Main/anime.js
@@ -1,20 +1,29 @@
 const input = require('input');
+const chalkAnimation = require('chalk-animation');
 const chalk = require('chalk');
 const anime = require('anime-dl');
 const boxen = require('boxen');
+const ProgressBar = require('progress');
 
-console.log(boxen("Welcome to Hera's Anime Download CLI console application", {padding: 1, margin: 1, borderStyle: 'double'}));
+let bar = new ProgressBar(':bar', { total: 50 });
+let timer = setInterval(function () {
+  bar.tick();
+  if (bar.complete) {
+      console.log(boxen("Welcome to Hera's Anime Download CLI console application", {padding: 1, margin: 1, borderStyle: 'double'}));
+      
+      async function animeDownload() {
+          const name = await input.text('What anime would you like to download? || ');
+          const chapter = await input.text('And now what episode would you like to download || ')
+          anime.getLinksByNameAndChapter(name, chapter).then((results)=>{
+              console.log(chalk.green(results.urls));
+            });
+        }
+        animeDownload();
+        
+    clearInterval(timer);
+  }
+}, 100);
 
-
-async function animeDownload() {
-    const name = await input.text('What anime would you like to download? || ');
-    const chapter = await input.text('And now what episode would you like to download || ')
-    anime.getLinksByNameAndChapter(name, chapter).then((data)=>{
-        console.log(chalk.green(data.urls));
-    });
-}
-
-animeDownload();
 
 /* 
 const anime = require('anime-dl')
